95bf5e811a7f75f88fb8f37437652f34318d4216
Carlos Donizete Froes <coringao@riseup.net>
2017-11-18 16:01:27 -0200

    Changed version sdl library

--- src/gamesound.h
+++ src/gamesound.h
@@ -1,16 +1,36 @@
+/*
+ *
+ * This file is part of JAG, Puzzle game where the goal is to break all
+ * the target pieces in each level and do this before the time runs out.
+ *
+ * Many thanks to XlabSoft & Ind. Infosystems, the originals authors of JAG.
+ *
+ * Copyright holder 2009-2012 Developed by XlabSoft & Industrial Infosystems
+ * Work continued by 2017 Carlos Donizete Froes [a.k.a coringao]
+ *
+ * JAG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * JAG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JAG. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef GAMESOUND_H
 #define GAMESOUND_H
 
 #include <QList>
 #include <QTimer>
 
-#ifdef Q_OS_WIN32
-#include "SDL.h"
-#include "SDL_mixer.h"
-#else
-#include "SDL/SDL.h"
-#include "SDL/SDL_mixer.h"
-#endif
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_mixer.h"
 
 class GameSound : public QObject
 {
