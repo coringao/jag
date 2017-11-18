047feac5781ed82bff1ac09b29496cbe64e989a9
Carlos Donizete Froes <coringao@riseup.net>
2017-11-18 16:06:45 -0200

    Updated and corrected an undefined variable

--- src/gamestatics.cpp
+++ src/gamestatics.cpp
@@ -1,3 +1,28 @@
+/*
+ *
+ * This file is part of JAG, Puzzle game where the goal is to break all
+ * the target pieces in each level and do this before the time runs out.
+ *
+ * Many thanks to XlabSoft & Ind. Infosystems, the originals authors of JAG.
+ *
+ * Copyright holder 2009-2012 Developed by XlabSoft & Industrial Infosystems
+ * Work continued by 2017 Carlos Donizete Froes [a.k.a coringao]
+ *
+ * JAG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * JAG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JAG. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "scene_if.h"
 #include "baseitem.h"
 
@@ -76,7 +101,7 @@ void IScene::removeBlock(PlaceInfo &pi, int row, int col)
 
   // create ghost
   createPixmapPopup(col2x(col), row2y(row), (qrand() % 3)-1, 10 + (qrand() % 5)-2,
-                              block1,
+                              b1 ? block1: block2,
                               50);
 }
 
