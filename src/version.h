c728e083771be555448f55ae720d0538b66e44dd
Carlos Donizete Froes <coringao@riseup.net>
2017-11-19 00:42:24 -0200

    Updated source code

--- src/version.h
+++ src/version.h
@@ -1,7 +1,32 @@
+/*
+ *
+ * This file is part of JAG, Puzzle game where the goal is to break all
+ * the target pieces in each level and do this before the time runs out.
+ *
+ * Many thanks to XlabSoft & Ind. Infosystems, the originals authors of JAG.
+ *
+ * Copyright holder 2009-2012 Developed by XlabSoft & Industrial Infosystems
+ * Work continued by 2017 Carlos Donizete Froes [a.k.a coringao]
+ *
+ * JAG is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * JAG is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JAG. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef VERSION_H
 #define VERSION_H
 
-#define GAME_VERSION        0x000302
-#define GAME_VERSION_STR    "0.3.2"
+#define GAME_VERSION        0x000303
+#define GAME_VERSION_STR    "0.3.3"
 
 #endif // VERSION_H
